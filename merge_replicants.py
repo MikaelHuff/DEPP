586888a5ba17b43f86f57e166296b1f217f0febc
Mikael Huff <mikaelkhuff@gmail.com>
2023-10-19 21:33:31 -0700

    fixed replicate=True breaking when no replicates

--- merge_replicants.py
+++ merge_replicants.py
@@ -22,17 +22,21 @@ def merge_replicants_in_dataframe(replicate_df, replicate_char='_'):
     new_df = pd.DataFrame()
     repeat_am = {}
     for row in replicate_df.index:
-        char_location = -(row[::-1].find(replicate_char) + 1)
-        row_new = row[:char_location]
-        if row_new not in new_df.index:
-            new_df = new_df.append(replicate_df.loc[row])
-            new_df = new_df.rename(index={row: row_new})
-            repeat_am[row_new] = 1
+        if replicate_char in row:
+            char_location = -(row[::-1].find(replicate_char) + 1)
+            row_new = row[:char_location]
+            if row_new not in new_df.index:
+                new_df = new_df.append(replicate_df.loc[row])
+                new_df = new_df.rename(index={row: row_new})
+                repeat_am[row_new] = 1
+            else:
+                new_df.loc[row_new] *= repeat_am[row_new]
+                new_df.loc[row_new] += replicate_df.loc[row]
+                new_df.loc[row_new] /= repeat_am[row_new]+1
+                repeat_am[row_new] += 1
         else:
-            new_df.loc[row_new] *= repeat_am[row_new]
-            new_df.loc[row_new] += replicate_df.loc[row]
-            new_df.loc[row_new] /= repeat_am[row_new]+1
-            repeat_am[row_new] += 1
+            new_df = new_df.append(replicate_df.loc[row])
+
 
     return new_df
 
@@ -44,12 +48,17 @@ def merge_replicants_in_seq(seq_file, output_file, replicate_char='_'):
     new_list = []
     new_dict = {}
     for row in seq_df.index:
-        char_location = -(row[::-1].find(replicate_char) + 1)
-        row_new = row[:char_location]
-        if row_new not in list(new_dict.keys()):
-            seq = SeqRecord(Seq(''.join(list(seq_df.loc[row]))), id=row_new, name=row_new, description=row_new)
+        if replicate_char in row:
+            char_location = -(row[::-1].find(replicate_char) + 1)
+            row_new = row[:char_location]
+            if row_new not in list(new_dict.keys()):
+                seq = SeqRecord(Seq(''.join(list(seq_df.loc[row]))), id=row_new, name=row_new, description=row_new)
+                new_list.append(seq)
+                new_dict[row_new] = 1
+        else:
+            seq = SeqRecord(Seq(''.join(list(seq_df.loc[row]))), id=row, name=row, description=row)
             new_list.append(seq)
-            new_dict[row_new] = 1
+
 
     SeqIO.write(new_list, output_file, 'fasta')
 
